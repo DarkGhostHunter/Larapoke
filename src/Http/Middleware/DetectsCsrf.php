009e160e37a83f78d4fb7a857adb01cedb045859
DarkGhostHunter <DarkGhostHunter@Gmail.com>
2018-12-24 21:32:28 -0300

    Refactored HTML detection

--- src/Http/Middleware/DetectsCsrf.php
+++ src/Http/Middleware/DetectsCsrf.php
@@ -7,14 +7,14 @@ use Illuminate\Http\Response;
 trait DetectsCsrf
 {
     /**
-     * Detect if the Response is OK and has form or CSRF Token
+     * Detect if the Response has form or CSRF Token
      *
      * @param $response
      * @return bool
      */
     protected function hasCsrf(Response $response)
     {
-        if (!$response->isOk() && strpos($response->headers->get('content-type'), 'text/html') === false) {
+        if (!$response->isOk() && $this->isHtml($response)) {
             return false;
         }
 
@@ -24,5 +24,16 @@ trait DetectsCsrf
         $hasCsrfInput = stripos($content, 'name="_token"');
 
         return $hasCsrfHeader || $hasCsrfInput;
+    }
+
+    /**
+     * Detect if the Response is HTML by its "content-type" header
+     *
+     * @param Response $response
+     * @return bool
+     */
+    protected function isHtml(Response $response)
+    {
+        return strpos($response->headers->get('content-type'), 'text/html') === false;
     }
 }

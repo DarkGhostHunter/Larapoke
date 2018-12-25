23ea72aa99fc0710b9439b3ae15c383f813ba139
DarkGhostHunter <DarkGhostHunter@Gmail.com>
2018-12-25 18:10:57 -0300

    Fixed Timeout script Added Browser Test Temporarily added custom fixed Orchestral/testbench-dusk to test Browsers under Windows.

--- resources/views/timeout.blade.php
+++ resources/views/timeout.blade.php
@@ -1,1 +1,1 @@
-setInterval(() => { if (new Date() - larapoke_date >= {{ $interval }} + {{ $lifetime }}) { location.reload(true); }},2000);
+setInterval(() => { if (new Date() - larapoke_date >= {{ $interval  }} * 1000 + {{ $lifetime }}) { location.reload(true); }},2000);
